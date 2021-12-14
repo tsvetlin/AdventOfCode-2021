47baadfd09db4e63bc048e40dc86c6874a92bfbe
Szvetlin Tanyi <svetlintanyi@gmail.com>
2021-12-14 20:59:52 +0100

    Day 7 Part 2

--- 07/07.js
+++ 07/07.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const crabPositions = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'}).split(',').map(element => +element)
 
 const max = Math.max(...crabPositions)
-const result = []
+let result = []
 
 for(let i = 0; i <= max; i++) {
   let fuelCost = 0
@@ -14,4 +14,22 @@ for(let i = 0; i <= max; i++) {
 }
 
 // part one solution
+console.log(Math.min(...result))
+
+result = []
+
+for(let i = 0; i <= max; i++) {
+  let totalFuelCost = 0
+  for(const crab of crabPositions){
+    const diff = Math.abs(i - crab)
+    let fuelCost = 0
+    for(let i = 1; i <= diff; i++){
+      fuelCost += i
+    }
+    totalFuelCost += fuelCost
+  }
+  result.push(totalFuelCost)
+}
+
+// part two solution
 console.log(Math.min(...result))
