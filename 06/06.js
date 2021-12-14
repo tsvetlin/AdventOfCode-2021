1177737048cc26828caa9e6e333481d521c09802
Szvetlin Tanyi <svetlintanyi@gmail.com>
2021-12-14 19:29:34 +0100

    Day 6 Part 2

--- 06/06.js
+++ 06/06.js
@@ -1,26 +1,64 @@
 const fs = require('fs');
 
+//let fishCount = 0
+
 function main(){
   const school = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'}).split(',').map(element => +element)
   //const school = [3,4,3,1,2]
-  for(let day = 1; day <=80; day++){
-    let born = 0
-    for(let i = 0; i< school.length; i++){
-      if(school[i] === 0){
-        school[i] = 6;
-        born++
+  const days = 256
+  const cycle = 7
+//5934 80 days
+//26 18 days
+  console.log('Here', days, cycle)
+  const one = kids(1, days, cycle) + 1
+  console.log('one', one)
+  const two = kids(2, days, cycle) + 1
+  console.log('two', two)
+  const three = kids(3, days, cycle) + 1
+  console.log('three', three)
+  const four = kids(4, days, cycle) + 1
+  console.log('four', four)
+  const five = kids(5, days, cycle) + 1
+  console.log('five', five)
 
-      } else {
-        school[i]--
-      }
-    }
-    for(let i = 0; i< born; i++){
-      school.push(8)
-    }
-  // console.log(`Day: ${day} ${school}`)
+ /* const result = {
+     one,
+     two,
+     three, 
+     four,
+     five
+  } */
+
+  const sum = [one, two, three, four, five]
+//  console.log(result)
+
+  let result = 0
+  for(let i = 0; i < school.length; i++){
+  // console.log(school[i], sum[school[i] -1])
+    result += sum[school[i] - 1]
   }
+  console.log(result)
+}
 
-console.log(school.length)
+  // Math.ceil((currentDay - initialAge) / cycle)
+const kids = (initialAge, currentDay, cycle) => {
+  let fishCount = 0
+  let day = currentDay - initialAge
+  const bornOnDay = []
+  while(day > 0){
+    bornOnDay.push(day)
+    day -= cycle
+  }
+  fishCount += bornOnDay.length
+  //console.log('FF', fishCount)
+  if(bornOnDay.length){
+//    console.log(bornOnDay)
+    for(let i = 0; i < bornOnDay.length; i++){
+      fishCount += kids(8 + 1, bornOnDay[i], cycle)
+    }
+
+}
+  return fishCount
 }
 
 main()
